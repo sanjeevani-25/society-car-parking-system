6521d04593e952954ea3606567b354c283d5f2ba
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2023-11-29 23:44:15 +0530

    ..

--- project.v
+++ project.v
@@ -1,36 +1,33 @@
 `define parking_slots 10
 
 module  add_passwords #(parameter N=`parking_slots)();
+
   integer fd,k,i;
   integer seed = 3;
   reg [$clog2(N):0] var [0:N];
   reg [$clog2(N):0] pwd;
 
-  initial begin  
-    
-    fd=$fopen("./input.txt","w");
+  fd=$fopen("./input.txt","w");
 
-    for (k=0; k<=N; k=k+1) begin 
-
-      pwd=$random(seed) % 20;
-      seed=$random;
-      $fwrite(fd," %b",pwd); 
-      // $fdisplay(fd,"%b",pwd); 
-      
-    end
+  for (k=0; k<=N; k=k+1) begin 
+    pwd=$random(seed) % 20;
+    seed=$random;
+    $fwrite(fd," %b",pwd); 
+    // $fdisplay(fd,"%b",pwd); 
     $display("Database updated");
-
-    $fclose(fd);
-
   end
+
+  $fclose(fd);
+
 endmodule
 
 module display_Allpass #(parameter N=`parking_slots)();
+
   output reg [$clog2(N):0] var [0:N];
   integer k;
-  initial begin
-    $readmemb("./input.txt", var);
-    $display("\nFlat Number : Password\n");
-    for (k=0; k<=N; k=k+1) $display("\t%0d \t\t: %b %d",k,var[k],var[k]);
-  end
+
+  $readmemb("./input.txt", var);
+  $display("\nFlat Number : Password");
+  for (k=0; k<=N; k=k+1) $display("\t%0d \t\t: %b %d",k,var[k],var[k]);
+
 endmodule
