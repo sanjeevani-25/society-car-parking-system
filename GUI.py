98a21f6f8077438e70ae4c8dc2237215a530d824
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2023-11-30 00:49:20 +0530

    ui

--- GUI.py
+++ GUI.py
@@ -1,16 +1,13 @@
-# import subprocess
-# proc = subprocess.Popen('cmd.exe', stdin=subprocess.PIPE)
-# print("run")
-# proc.stdin.write(b"python --version")
-# proc.stdin.flush()
-
 import os
+
+open('DB.txt', 'w').close()
+open('input.txt', 'w').close()
 
 # flat_number=input("Enter Your Flat Number : ")
 # password=input("Enter Your Password : ")
 
+# file = open("input.txt", "r")
 # print(flat_number,password)
 
-# os.system('cmd /c "iverilog --v"')
 os.system('cmd /c "iverilog -o main_out.vvp main.v"')
 os.system('cmd /c "vvp main_out.vvp"')
--- modules.v
+++ modules.v
@@ -3,17 +3,17 @@
 module  add_passwords #(parameter N=`parking_slots)();
 
   integer fd,k,i;
-  integer seed = 3;
+  integer seed = 10;
   reg [$clog2(N):0] var [0:N];
   reg [$clog2(N):0] pwd;
 
 initial begin
   
-  fd=$fopen("./input.txt","w");
+  fd=$fopen("./DB.txt","w");
 
   for (k=0; k<=N; k=k+1) begin 
-    pwd=$random(seed) % 20;
-    seed=$random;
+    pwd=$urandom(seed) % 20;
+    seed=$urandom;
     $fwrite(fd," %b",pwd); 
     // $fdisplay(fd,"%b",pwd); 
   end
@@ -31,7 +31,7 @@ module display_Allpass #(parameter N=`parking_slots)();
 
 initial begin
   
-  $readmemb("./input.txt", var);
+  $readmemb("./DB.txt", var);
   $display("\nFlat Number : Password");
   for (k=0; k<=N; k=k+1) $display("\t%0d \t\t: %b %d",k,var[k],var[k]);
 end
